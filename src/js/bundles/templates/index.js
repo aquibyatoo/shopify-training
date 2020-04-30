dafd06fc8db4fa9537a785a0aa0548bd01dde599
Anar Babashov <anar@anattadesign.com>
2020-04-30 15:56:59 -0700

    Updated eslint and stylelint

--- src/js/bundles/templates/index.js
+++ src/js/bundles/templates/index.js
@@ -1,16 +1,1 @@
 import 'Styles/templates/index.scss';
-
-
-// ES5 syntax
-function Person() {
-    // we assign `this` to `self` so we can use it later
-    var self = this;
-    self.age = 0;
-
-    setInterval(function growUp() {
-        // `self` refers to the expected object
-        self.age++;
-    }, 1000);
-}
-
-var p = new Person();
