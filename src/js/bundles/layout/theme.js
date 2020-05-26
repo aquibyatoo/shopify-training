811905fc03426c26b233436c36075387d5a7c88c
Anar Babashov <anar@anattadesign.com>
2020-05-26 16:20:50 -0700

    Added lazysizes plugin, updated class for images

--- src/js/bundles/layout/theme.js
+++ src/js/bundles/layout/theme.js
@@ -1,4 +1,12 @@
+import 'lazysizes/plugins/object-fit/ls.object-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes';
+import 'lazysizes/plugins/respimg/ls.respimg';
+
 import 'Styles/layout/theme.scss';
+
 import navigation from '../components/navigation';
 
 navigation();
