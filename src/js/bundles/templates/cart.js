7c9d372e39b60d7dde788ce5751f14830b2e3e90
Anar Babashov <anar@anattadesign.com>
2020-05-27 18:09:17 -0700

    Added cart page layout

--- src/js/bundles/templates/cart.js
+++ src/js/bundles/templates/cart.js
@@ -1,1 +1,41 @@
 import 'Styles/templates/cart.scss';
+
+(function() {
+  'use strict';
+
+  let count = 1;
+  const dom = {};
+
+  const cacheDom = () => {
+    dom.countEl = document.getElementById('count');
+    dom.countMinus = document.getElementById('mins');
+    dom.countPlus = document.getElementById('plus');
+  }
+
+  const plus = () => {
+    count++
+    dom.countEl.value = count
+    console.log(count);
+  }
+
+  const minus = () => {
+    if (count > 1) {
+      count--
+      dom.countEl.value = count
+    }
+    console.log(count);
+  }
+
+  const bindUIActions = () => {
+    dom.countMinus.addEventListener('click', minus);
+    dom.countPlus.addEventListener('click', plus);
+  }
+
+  const init = () => {
+    cacheDom();
+    bindUIActions();
+  }
+
+  init();
+
+})();
