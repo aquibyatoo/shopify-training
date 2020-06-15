080fee6e3251970446fa023af6c72d3fad2a3381
Anar Babashov <anar@anattadesign.com>
2020-06-15 12:54:08 -0700

    added ajax search, added styles for result list

--- src/js/bundles/components/ajax-search.js
+++ src/js/bundles/components/ajax-search.js
@@ -9,26 +9,39 @@ export default () => {
 
     const cacheDom = () => {
       dom.currentAjaxRequest = null;
-      dom.resultsList = $('.search-results');
+      dom.resultsList = document.querySelectorAll('.search-results');
+      dom.searchInput = document.getElementById('searchInput');
     }
 
-    $('form[action="/search"]').each(function () {
-      var input = $(this).find('input[name="q"]');
+    // Each search form.
+    $('form[action="/search"]').each(function() {
+      const input = $(this).find('input[name="q"]');
+      // We handle input on keyup.
       input.attr('autocomplete', 'off').bind('keyup change', function () {
         var term = $(this).val();
         var searchURL = '/search/suggest.json?q=' + term + '&resources[type]=product'
         dom.resultsList = $('.search-results');
+        // if input is empty, we hide previous search.
+        if (!term && term === '') {
+          dom.resultsList.slideUp(100);
+        }
+        // Checking input search term.
         if (term.length > 3 && term !== $(this).attr('data-old-term')) {
           $(this).attr('data-old-term', term);
           if (dom.currentAjaxRequest !== null) dom.currentAjaxRequest.abort();
+          // Make ajax request
           dom.currentAjaxRequest = $.getJSON(searchURL, function (data) {
             var response = data.resources.results.products;
             dom.resultsList.empty();
+            // Cancel we show no result and hide it.
             if (response.length === 0) {
-              dom.resultsList.html('<p>No results.</p>');
-              dom.resultsList.fadeIn(100);
-              dom.resultsList.hide();
+              dom.resultsList.html('<span>No results...</span>');
+              dom.resultsList.slideDown(100);
+              setTimeout(() => {
+                dom.resultsList.slideUp(100);
+              }, 2000);
             } else {
+              // Status 200, we build search list.
               $.each(response, function (index, item) {
                 var link = $('<a></a>').attr('href', item.url);
                 link.append('<span class="thumbnail"><img width="50" src="' + item.image + '" /></span>');
@@ -36,10 +49,7 @@ export default () => {
                 link.wrap('<div class="item-row"></div>');
                 dom.resultsList.append(link.parent());
               });
-              if (response.results_count > 10) {
-                dom.resultsList.append('<div class="row"><div class="columns"><a class="btn" href="' + searchURL + '"> +(' + data.results_count + ') more</a></div></div>');
-              }
-              dom.resultsList.fadeIn(200);
+              dom.resultsList.slideDown(100);
             }
           });
         }
